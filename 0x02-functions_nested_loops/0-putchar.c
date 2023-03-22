111062bd647aa7120408a5afd1d28964a079ee2f
moatzhamed <moatzhamed@hotmail.com>
2023-03-21 20:40:24 -0700

    Projcet on nested loop in C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: A C program that prints with putchar
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
