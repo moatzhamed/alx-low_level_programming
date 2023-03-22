f39e7a55513fb445f6b2790f14d00829cd0dd3ea
moatzhamed <moatzhamed@hotmail.com>
2023-03-22 06:21:02 -0700

    Nested loop project in C

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,10 +5,10 @@
  *
  */
 
- void print_alphabet(void)
+ void print_alphabet_x10(void);
 {
 	int i, ch;
-	for (i=1; i<=10; i++)
+	for (i=1; i<=1; i++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
