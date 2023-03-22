7787f1e22ceae13d4c994149c627d1259fd8344d
moatzhamed <moatzhamed@hotmail.com>
2023-03-22 14:15:34 -0700

    Nested loop project in C

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function print the last digit in the input number
+ *
+ * @n : input number
+ *
+ * Return: value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int lastdigit;
+	if (n < 0)
+		lastdigit = (-1) * (n % 10);
+	else
+		lastdigit = n % 10;
+
+	_putchar(lastdigit + '0');
+	return (lastdigit);
+}
+
+
+			
