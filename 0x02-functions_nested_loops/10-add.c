b770bad20583b19c96cf8269a6ccee5fde466387
moatzhamed <moatzhamed@hotmail.com>
2023-03-22 19:13:38 -0700

    Nested loop project in C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @x : first input
+ * @y : second input
+ *
+ * Return: sum of x & y
+ */
+
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
