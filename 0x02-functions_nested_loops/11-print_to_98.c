0fe8abbfc4f9c8a8a7d0b2c80e64d0553eff3897
moatzhamed <moatzhamed@hotmail.com>
2023-03-22 19:41:32 -0700

    Nested loop project in C

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98
  *
- * @n: first input 
+ *@n: first input
  */
 
 void print_to_98(int n)
@@ -11,10 +11,10 @@ void print_to_98(int n)
 	int count;
 
 	if (n > 98)
-		for (count = n; count > 98; count --)
+		for (count = n; count > 98; count--)
 			printf("%d, ", count);
 	else
-		for (count = n; count < 98; count ++)
+		for (count = n; count < 98; count++)
 			printf("%d, ", count);
 	printf("98\n");
 }
