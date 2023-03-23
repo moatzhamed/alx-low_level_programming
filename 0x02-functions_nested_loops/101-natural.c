9aa5392ebb3e82cfb6165c170814c76d4f1852d2
moatzhamed <moatzhamed@hotmail.com>
2023-03-22 19:58:36 -0700

    Nested loop project in C

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * main - computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded),
+ * main - computes and prints the sum of all
+ *	the multiples of 3 or 5 below 1024 (excluded),
  *
  * Return: Always 0.
  */
