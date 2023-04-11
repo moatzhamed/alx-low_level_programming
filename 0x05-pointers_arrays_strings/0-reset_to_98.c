7c2328ff9047d233d6bde881e523effc6989d84f
moatzhamed <moatzhamed@hotmail.com>
2023-04-11 08:59:55 -0700

    Pointer Project

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
-* reset_to_98 - takes a pointer to an int as parameter 
-* 		and updates the value it points to to 98
+* reset_to_98 - takes a pointer to an int as parameter
+*		and updates the value it points to to 98
 *
-* #n : int varaible
+* @n : int varaible
 *
 * Retrun: nothing
 */
 
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
